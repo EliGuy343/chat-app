0b1d25b761d54bc829593bbc6be82cf07588b5bb
Eli Guy <eliguy343@gmail.com>
2022-06-11 16:13:24 +0300

    Chat-app init

---
+++ server.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql} from "apollo-server";
+
+const users = [
+    {
+        id:1,
+        name:"Ivan Denisovich",
+        email:'IvanD15@gmail.com',
+        password:'12345'
+    },
+    {
+        id:2,
+        name:"Joe Pisser",
+        email:'JP',
+        password:'12345'
+    }
+]
+
+const typeDefs = gql`
+    type Query {
+        users:[User]
+        user(id:ID):User
+    }
+
+    type User {
+        id:ID
+        name:String
+        email:String
+    }
+`
+
+const resolvers = {
+    Query: {
+        users:() => users,
+        user:(parent, args, context) => {
+            console.log(args.id);
+            return users.find(item => item.id == args.id);
+        }
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({url})=> {
+    console.log(`server ready at ${url}`);
+})
