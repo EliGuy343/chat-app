6badb65cf342e000dde0d46bb48feb1928840900
Eli Guy <eliguy343@gmail.com>
2022-06-20 17:30:08 +0300

    added: send message

--- client/src/components/ChatScreen.jsx
+++ client/src/components/ChatScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   AppBar,
@@ -7,17 +7,35 @@ import {
   CircularProgress,
   TextField,
   Toolbar,
-  Typography
+  Typography,
+  Stack
 } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import MessageCard from './MessageCard';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_MESSAGES, GET_USER } from '../graphql/queries';
+import { useEffect } from 'react';
+import { SEND_MESSAGE } from '../graphql/mutations';
 const ChatScreen = () => {
     const {id} = useParams();
+    useEffect(() => {
+      if(data)
+        setMessages(data.messagesByUser);
+    },[id])
+    const [messages, setMessages] = useState([]);
+    const [text, setText] = useState();
     const {data:userData, loading:userLoading, error:userError} = useQuery(
-      GET_USER,{ variables:{receiverId:+id}});  
+      GET_USER,{ variables:{receiverId:+id}});
     const {data, loading , error} = useQuery(
-      GET_MESSAGES,{variables:{receiverId:+id}});
+        GET_MESSAGES,{variables:{receiverId:+id}, onCompleted(data) {
+          setMessages(data.messagesByUser);
+    }});
+    const [sendMessage] = useMutation(SEND_MESSAGE, {
+      onCompleted(data) {
+        console.log(messages);
+        setMessages([...messages,data.createMessage])
+      }
+    })
     return (
         <Box flexGrow={1}>
           {userLoading ? (
@@ -51,24 +69,48 @@ const ChatScreen = () => {
             </Box>
           ) : (
           <Box backgroundColor='#e6e6e6' height='80vh' overflow='auto'>
-            {data.messagesByUser.map(msg=>
+            {messages.map(msg=>
               <MessageCard
                 key={msg.createdAt} 
                 text={msg.text} 
                 date={msg.createdAt} 
-                direction={msg.receiverId == +id ? 'end' : 'start'}
+                direction={msg.receiverId === +id ? 'end' : 'start'}
               />)
             }
           </Box>
           )}
-          <TextField
-            placeholder='Enter a Message'
-            variant='outlined'
-            background='#f5f5f5'
-            fullWidth
-            multiline
-            rows={2}
-          />
+          <Stack direction='row'>
+            <TextField
+              placeholder='Enter a Message'
+              variant='outlined'
+              background='#f5f5f5'
+              fullWidth
+              multiline
+              rows={2}
+              value={text}
+              onKeyDown={(e)=>{
+                if(e.key === 'Enter') {
+                  setText('');
+                  sendMessage({
+                    variables:{
+                      receiverId: +id,
+                      text
+                    }
+                  });
+                }
+              }}
+              onChange={e=>setText(e.target.value)}
+            />
+            <SendIcon fontSize='large' onClick={()=>{
+              setText('');
+              sendMessage({
+                variables:{
+                  receiverId: +id,
+                  text
+                }
+              });
+            }}/>
+          </Stack>
         </Box>
   )
 }
--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -3,16 +3,28 @@ import {gql} from '@apollo/client';
 export const SIGNUP_USER = gql`
     mutation SignupUser($newUser: UserInput!) {
         signupUser(newUser: $newUser) {
-        id
-        name
-        email
+            id
+            name
+            email
+        }
     }
-}`;
+`
 
 export const LOGIN_USER = gql`
     mutation Mutation($userSignin: UserSigninInput) {
         signinUser(userSignin: $userSignin) {
             token
         }
-}
+    }
+`
+export const SEND_MESSAGE = gql`
+    mutation CreateMessage($receiverId: Int!, $text: String!) {
+        createMessage(receiverId: $receiverId, text: $text) {
+            id
+            text
+            receiverId
+            senderId
+            createdAt
+        }
+    }
 `
